e87e9cdcf929cfa82880ab44a1ecc993c2cb7da2
jayeshpatil01 <jayeshpatil8041@gmail.com>
2022-07-11 23:32:51 +0530

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'load_map', to: 'staticpages#index'
+  root to: 'staticpages#index'
   
   get 'load_geo_json_data', to: 'staticpages#load_geo_json_data'
 
